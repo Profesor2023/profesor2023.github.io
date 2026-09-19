fe22cde6afbdfd75fc9548b1b333b765e9ea1100
Profesor2023 <132255444+Profesor2023@users.noreply.github.com>
2026-09-19 19:54:16 -0300

    Renovar caché de la identidad visual

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* Tejer la red - service worker
    Guarda una copia del sitio para usarlo sin conexion. */
-var CACHE='tejer-la-red-v1-6-identidad-4';
+var CACHE='tejer-la-red-v1-6-identidad-5';
 var BASICOS=[
- './','./index.html','./estilo.css','./manifest.webmanifest',
+ './','./index.html','./estilo.css?v=trama-abierta-1','./manifest.webmanifest',
  './tejer-la-red/','./tejer-la-red/index.html',
  './recursos/','./recursos/index.html',
  './temas/','./temas/index.html',
